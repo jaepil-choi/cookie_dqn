c953c42e8faec04012e2346d8b36e35fc88b54dc
Jaepil.Choi <chljeffreyz@gmail.com>
2018-05-31 15:49:07 +0900

    make mat_generator.rmatrix to produce matrixes for testing. (size 12*9)

---
+++ mat_generator.py
@@ -0,0 +1,12 @@
+import random
+
+# 12*9의 우리 grid와 동일한 크기의 2차원 list matrix를 뽑는다.
+def rmatrix():
+    matrix = []
+    for _ in range(9):
+        rand = [random.choice(range(5)) for _ in range(12)]
+        matrix.append(rand)
+
+    return matrix
+
+
